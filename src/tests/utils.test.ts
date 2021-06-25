365a2cb335b1e2f69998b84addcbef89834e2d53
jialei qin <jialei.qin@thoughtworks.com>
2021-06-25 21:51:31 +0800

    chore: add test

---
+++ src/tests/utils.test.ts
@@ -0,0 +1,9 @@
+import { getKey } from '../utils';
+
+describe('utils', () => {
+  describe('getKey', () => {
+    it('should return b', () => {
+      expect(getKey('a/b')).toBe('b')
+    });
+  });
+});
