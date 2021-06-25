3604f6fbcffec5c4a7bfa4053cb37aa9bb265ac0
jialei qin <jialei.qin@thoughtworks.com>
2021-06-25 22:01:45 +0800

    refactor: reformat

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,46 @@
 import produce from 'immer';
-import {Provider,useSelector} from "react-redux";
+import { Provider, useSelector } from 'react-redux';
 import { applyMiddleware, combineReducers, createStore, Store } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { NAME_SPACE_FLAG } from './constant';
-import {
-  HandleActionMap,
-  HandleReducerMap,
-  RunParams,
-  RunResult
-} from './types';
+import { HandleActionMap, HandleReducerMap, RunParams, RunResult } from './types';
 import { getKey } from './utils';
 
 // eslint-disable-next-line functional/no-let
-let _store: any = {}
-const _actionMap: any = {}
+let _store: any = {};
+const _actionMap: any = {};
 
 const REDUCER_KEY = 'reducer';
 
 
- const processReducerModules = <ReducerMap>(reducerModules: any) => {
-  const obj = {} as any
+const processReducerModules = <ReducerMap>(reducerModules: any) => {
+  const obj = {} as any;
   Object.keys(reducerModules).forEach(reducerName => {
     // eslint-disable-next-line functional/immutable-data
-    obj[reducerName] = createModel(reducerModules[reducerName])
-  })
+    obj[reducerName] = createReducerModule(reducerModules[reducerName]);
+  });
   const reducers = getReducerMap<ReducerMap>(obj);
 
   return {
     reduxBriefModules: obj,
     reducers,
-    actionMap:_actionMap as HandleActionMap<ReducerMap>
-  }
-}
+    actionMap: _actionMap as HandleActionMap<ReducerMap>
+  };
+};
 
 const getActionMap = (reducerModule: { readonly [x: string]: any }, namespace: string) =>
   Object.keys(reducerModule).reduce((actionMap, actionName) => {
     const actionNameWithNamespace = namespace + NAME_SPACE_FLAG + actionName;
-    generateActionMap(namespace, actionName, actionNameWithNamespace)
+    generateActionMap(namespace, actionName, actionNameWithNamespace);
     return {
       ...actionMap,
       [actionName]: (payload: any) => {
         _store.dispatch({
           type: actionNameWithNamespace,
-          payload,
+          payload
         } as never);
-      },
+      }
     };
   }, {});
 
@@ -55,22 +50,27 @@ const generateActionMap = (moduleName: string, actionName: string, actionNameWit
   _actionMap[moduleName] = {
     ..._actionMap[moduleName],
     [actionName]: actionNameWithNamespace
-  }
-}
+  };
+};
 
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-const withReducerModule = ({state, action, reducer, namespace = ''}) =>
+const withReducerModule = ({ state, action, reducer, namespace = '' }) =>
   Object.keys(reducer)
     .map((key) => namespace + NAME_SPACE_FLAG + key)
     .includes(action.type)
     ? produce(state, (draft: any) => reducer[getKey(action.type)](action.payload, draft))
     : state;
 
- const createModel = (model: any) => {
-  const {reducer, namespace} = model;
-  const reducerModule = (state = model.state, action: any) => withReducerModule({state, action, reducer, namespace});
+const createReducerModule = (model: any) => {
+  const { reducer, namespace } = model;
+  const reducerModule = (state = model.state, action: any) => withReducerModule({
+    state,
+    action,
+    reducer,
+    namespace
+  });
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   // eslint-disable-next-line functional/immutable-data
@@ -79,11 +79,11 @@ const withReducerModule = ({state, action, reducer, namespace = ''}) =>
 };
 
 
- const mountReducerModules = (
+const mountReducerModules = (
   store: { readonly [x: string]: any },
   reducerModules: { readonly [x: string]: { readonly [x: string]: any } }
 ) => {
-  _store = store
+  _store = store;
   Object.keys(reducerModules).forEach((moduleName) => {
     // eslint-disable-next-line functional/immutable-data
     _store[moduleName] = reducerModules[moduleName][REDUCER_KEY];
@@ -91,8 +91,7 @@ const withReducerModule = ({state, action, reducer, namespace = ''}) =>
 };
 
 
-
- const getReducerMap = <ReducerMap>(betterReduxModules: any): HandleReducerMap<ReducerMap> => {
+const getReducerMap = <ReducerMap>(betterReduxModules: any): HandleReducerMap<ReducerMap> => {
   const obj = {} as any;
   Object.keys(betterReduxModules).forEach((moduleName) => {
     // eslint-disable-next-line functional/immutable-data
@@ -101,24 +100,24 @@ const withReducerModule = ({state, action, reducer, namespace = ''}) =>
   return obj as HandleReducerMap<ReducerMap>;
 };
 
- const run= <T>(options:RunParams<T>):RunResult<T> =>{
-   const {modules,middlewares=[]} =options
+const run = <T>(options: RunParams<T>): RunResult<T> => {
+  const { modules, middlewares = [] } = options;
 
-   const {reduxBriefModules,reducers,actionMap} = processReducerModules<T>(modules)
-   const rootReducer = combineReducers(reduxBriefModules);
-   const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares))) as Store; // todo 环境变量
-   mountReducerModules(store, reduxBriefModules);
+  const { reduxBriefModules, reducers, actionMap } = processReducerModules<T>(modules);
+  const rootReducer = combineReducers(reduxBriefModules);
+  const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares))) as Store; // todo 环境变量
+  mountReducerModules(store, reduxBriefModules);
 
-   return {
-     store,
-     reducers,
-     selectors:{},
-     actions:actionMap//todo rename actions
-  }
- }
+  return {
+    store,
+    reducers,
+    selectors: {},
+    actions: actionMap//todo rename actions
+  };
+};
 
 export {
   run,
   useSelector,
   Provider
-}
+};
