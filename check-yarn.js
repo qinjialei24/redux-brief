28794560e05aaf462ff55efa3ffbdf64c61950f3
qinjialei24 <954551222@qq.com>
2024-08-30 20:32:32 +0800

    refactor: use pnpm

--- check-yarn.js
+++
@@ -1,7 +0,0 @@
-if (!/yarn\.js$/.test(process.env.npm_execpath || '')) {
-  // eslint-disable-next-line no-console
-  console.warn(
-    '\u001b[33mThis repository requires Yarn 1.x for scripts to work properly.\u001b[39m\n'
-  );
-  process.exit(1);
-}
--- src/tests/utils.test.ts
+++
@@ -1,9 +0,0 @@
-import { getKey } from '../utils';
-
-describe('utils', () => {
-  describe('getKey', () => {
-    it('should return b', () => {
-      expect(getKey('a/b')).toBe('b');
-    });
-  });
-});
